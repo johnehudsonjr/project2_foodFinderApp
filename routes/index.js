5bd96d40996c68ce2241819e49f9948fcf316b61
johnehudsonjr <jeh.hudson@hotmail.com>
2019-01-11 14:52:19 -0500

    YIKES

--- routes/index.js
+++ routes/index.js
@@ -5,29 +5,34 @@ const shareLocationController = require('../controllers/shareLocationController'
 const addFoodController = require('../controllers/addFoodController')
 const subscriberListController = require('../controllers/subscriberListController')
 
-// findfood
-router.get('/findfood', shareLocationController.index2)
-router.get('/findfood', shareLocationController.new2)
 
 router.get('/subscribe', subscriberListController.index)
 router.post('/subscribe', subscriberListController.create)
+
+
+// findfood
+
+router.get('/findfood', shareLocationController.new2) 
+
 
 // Share Location
 router.get('/', shareLocationController.index)
 router.get('/sharefood', shareLocationController.index2)
 router.get('/sharefood/new', shareLocationController.new)
 router.post('/', shareLocationController.create) 
+
+router.get('/findfood', shareLocationController.show)
+
 router.get('/:id', shareLocationController.show)
 router.get('/:id/update', shareLocationController.edit)
 router.patch('/:id', shareLocationController.update)
 router.delete('/:id', shareLocationController.delete)
 
+
+
 // Add Food
 router.get('/:id/addfood', addFoodController.show)
 router.post('/:id/addfood', addFoodController.create)
-
-// DELETE FOOD
-
 
 // TESTING
 router.delete('/:id/:newFood', addFoodController.delete)
